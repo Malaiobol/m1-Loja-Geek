ca4b31682b5cbd527220255c4b3172c38c680f7f
Edmilson <wow_ju1@hotmail.com>
2022-09-05 08:39:49 -0300

    alteração no código JS e HTML, trocando de lugar a lista de itens

--- script.js
+++ script.js
@@ -7,55 +7,7 @@ const secaoListPainting = document.querySelector('.paintings');
 //LISTA ATUAL ''PINTURA''//
 
 let listPainting = [
-    {
-        Imagem: "./assets/img/actions/animewoman.jpg",
-        Nome: 'Asuka',
-        Valor: 'R$125,00',
-    },
-    {
-        Imagem: "./assets/img/actions/dragonballpersonagem.jpg",
-        Nome: 'Goku Super Sayajin',
-        Valor: 'R$309,00',
-    },
-    {
-        Imagem: "./assets/img/actions/starwarspersonagem.jpg",
-        Nome: 'Baby Yoda',
-        Valor: 'R$276,00',
-    }
-]
-
-//FABRICA ITEM ''PINTURA''//
-
-function createPaitingItem(objeto) {
-    for(let i = 0; i < objeto.length; i++){
-        const elementoLista = document.createElement('li')
-
-        let img = document.createElement('img')
-            img.src = objeto[i].Imagem
-            img.alt = objeto[i].Nome
-        let nome = document.createElement('p')
-        let valor = document.createElement('span')
-
-        elementoLista.classList.add('card')
-
-        nome.innerText = `${objeto[i].Nome}`
-        valor.innerText = `${objeto[i].Valor}`
-
-        elementoLista.appendChild(img)
-        elementoLista.appendChild(nome)
-        elementoLista.appendChild(valor)
-
-        listPaintings.appendChild(elementoLista)
-
-    }
-}
-createPaitingItem(listPainting)
-
-//LISTA FIGURE ACTION//
-
-let listFigure = [
-    //repeti o mesmo item para ativar o overflow
-    {
+{
         Imagem: "./assets/img/painting/clock.jpg",
         Nome: 'Clock',
         Valor: 'R$50,00',
@@ -115,6 +67,55 @@ let listFigure = [
         Nome: 'C3-PO',
         Valor: 'R$150,00',
     }
+]
+
+//FABRICA ITEM ''PINTURA''//
+
+function createPaitingItem(objeto) {
+    for(let i = 0; i < objeto.length; i++){
+        const elementoLista = document.createElement('li')
+
+        let img = document.createElement('img')
+            img.src = objeto[i].Imagem
+            img.alt = objeto[i].Nome
+        let nome = document.createElement('p')
+        let valor = document.createElement('span')
+
+        elementoLista.classList.add('card')
+
+        nome.innerText = `${objeto[i].Nome}`
+        valor.innerText = `${objeto[i].Valor}`
+
+        elementoLista.appendChild(img)
+        elementoLista.appendChild(nome)
+        elementoLista.appendChild(valor)
+
+        listPaintings.appendChild(elementoLista)
+
+    }
+}
+createPaitingItem(listPainting)
+
+//LISTA FIGURE ACTION//
+
+let listFigure = [    
+    {
+        Imagem: "./assets/img/actions/animewoman.jpg",
+        Nome: 'Asuka',
+        Valor: 'R$125,00',
+    },
+    {
+        Imagem: "./assets/img/actions/dragonballpersonagem.jpg",
+        Nome: 'Goku Super Sayajin',
+        Valor: 'R$309,00',
+    },
+    {
+        Imagem: "./assets/img/actions/starwarspersonagem.jpg",
+        Nome: 'Baby Yoda',
+        Valor: 'R$276,00',
+    }
+    //repeti o mesmo item para ativar o overflow
+    
 ]
 
 //FABRICA ITENS FIGURE ACTION//
